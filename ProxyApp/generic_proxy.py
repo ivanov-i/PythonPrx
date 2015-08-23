a2570e6ab2c874ee27aacbae88a48e6c97f058dd
ivanov-i <ivanov.i@gmail.com>
2015-08-23 14:00:45 +0600

    Proxy Listens

--- ProxyApp/generic_proxy.py
+++ ProxyApp/generic_proxy.py
@@ -1,6 +1,10 @@
-class GenericProxy(object):
-    def __init__(self):
-        print("aaaa")
+import socket
 
-    def a(self):
-        print("asdf")
+class GenericProxy(object):
+    def __init__(self, address):
+        self.address = address
+
+    def start(self):
+        self.socket = socket.socket()
+        self.socket.bind(self.address)
+        self.socket.listen(10)
--- Tests/test_proxy.py
+++ Tests/test_proxy.py
@@ -1,8 +1,12 @@
 from unittest import TestCase
+import socket
 
 from ProxyApp.generic_proxy import GenericProxy
 
-
 class TestProxy(TestCase):
-    def test(self):
-        proxy = GenericProxy()
+    def test_that_listens_on_some_port(self):
+        address = ('127.0.0.1', 8333)
+        proxy = GenericProxy(address)
+        proxy.start()
+        client = socket.create_connection(address)
+        client.close()
